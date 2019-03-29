a7da5b2194bdca70a460f952808feb2b9f2ce9ac
jizel <jizelgm98@gmail.com>
2019-03-29 21:19:15 +0100

    function

---
+++ calcul.js
@@ -0,0 +1,5 @@
+function un (){
+    var x=document.getelementbyld("un").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
